656d295a7b27af6187159dd765e298733d7c908a
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2025-11-12 17:48:50 -0500

    write basic install script for server

--- install.sh
+++ install.sh
@@ -3,4 +3,14 @@ set -euo pipefail
 
 echo "Running install.sh..."
 
+if [ -d ".venv" ]; then
+	rm -rf .venv
+fi
+
+python3 -m venv .venv
+# shellcheck disable=SC1091
+. .venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -e .
+
 echo "Installation complete!"
